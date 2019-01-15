427192d0ddf8d1e3cec28d594496045750ecb187
delphinpro <delphinpro@gmail.com>
2019-01-15 17:46:54 +0400

    Подключение отладочной панели в режиме разработки

---
+++ src/main.js
@@ -0,0 +1,23 @@
+/**
+ * WofhTools
+ * @author      delphinpro <delphinpro@gmail.com>
+ * @copyright   copyright © 2019 delphinpro
+ * @license     licensed under the MIT license
+ */
+
+import Vue from 'vue';
+
+
+Vue.config.productionTip = false;
+
+if (process.env.NODE_ENV === 'development') {
+    const Debugger = () => import('@/components/Debugger');
+    const dbg = document.createElement('div');
+    dbg.setAttribute('id', 'debugger');
+    document.documentElement.appendChild(dbg);
+
+    new Vue({
+        el: '#debugger',
+        render: h => h(Debugger),
+    });
+}
