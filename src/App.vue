8b7ff5112c031519502b450a54b030656b187567
delphinpro <delphinpro@gmail.com>
2019-01-15 18:20:53 +0400

    Подвал AppFooter

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,22 @@
  * @license     licensed under the MIT license
  */
 
+import AppFooter from '@/components/AppFooter';
+
 export default {
+    components: {
+        AppFooter,
+    },
+
     data: () => ({}),
 };
 </script>
 
 <template>
     <div id="app" class="app">
+        <div class="app__navbar"></div>
+        <div class="app__main"></div>
+        <AppFooter class="app__footer"/>
     </div>
 </template>
 
@@ -20,5 +29,21 @@ export default {
         min-height: 100vh;
         display: flex;
         flex-direction: column;
+
+        &__navbar,
+        &__main,
+        &__footer {
+            width: 100%;
+        }
+
+        &__navbar {
+        }
+
+        &__main {
+            flex-grow: 1;
+        }
+
+        &__footer {
+        }
     }
 </style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,81 @@
+<script>/**
+ * WofhTools
+ * @author      delphinpro <delphinpro@gmail.com>
+ * @copyright   copyright © 2019 delphinpro
+ * @license     licensed under the MIT license
+ */
+
+export default {
+    data: () => ({}),
+    computed: {
+        projectName() { return 'WofhTools'; },
+        projectVer() { return '4.0'; },
+    },
+};
+</script>
+
+<template>
+    <div class="footer">
+        <div class="footer__container container">
+            <div class="footer__section">
+                <p>
+                    © {{projectName}}, v{{projectVer}},
+                    <a href="http://delphinpro.ru" target="_blank" rel="noreferrer">delphinpro</a>,
+                    2013—2019 гг.
+                </p>
+                <p>
+                    <a href="#">Last update: xx.xx.xxxx, xx:xx</a>
+                </p>
+            </div>
+            <div class="footer__section">
+                <img src="../assets/main/counter.png" alt="">
+            </div>
+            <div class="footer__section text-md-right">
+                <ul>
+                    <li><a href="#">Обратная связь</a></li>
+                    <li><a href="#">Карта сайта</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    .footer {
+        @include font(11px, 14px);
+        width: 100%;
+        background: $background-dark-2;
+        box-shadow: $shadow-bar inset;
+
+        &__container {
+            @include media-breakpoint-up(md) {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+            }
+        }
+
+        &__section {
+            padding: 1em 0;
+        }
+
+        p {
+            margin: 0;
+        }
+
+        a {
+            color: inherit;
+
+            &:hover {
+                color: lighten($main-color, 10%);
+            }
+        }
+
+        ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+            display: inline-flex;
+        }
+    }
+</style>
