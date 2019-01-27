5329f1a8642ab0b1031cdc636e11bcba46783d71
delphinpro <delphinpro@gmail.com>
2019-01-27 05:14:13 +0400

    Минимально стилизована страница 404

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,5 +1,16 @@
 <template>
-    <div class="about">
-        <h1>Error 404: page not found</h1>
+    <div class="container page-404">
+        <h1>Ошибка 404: страница не найдена</h1>
+        <p>Вы перешли на страницу, которой не существует, либо она была удалена.</p>
+        <p><router-link to="/">Перейти на главную</router-link></p>
     </div>
 </template>
+
+<style lang="scss">
+    .page-404 {
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+</style>
