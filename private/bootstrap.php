e2df4e7f46223eb5a10e3b4b744388109ae9f021
delphinpro <delphinpro@gmail.com>
2019-01-28 17:32:55 +0400

    Определение констант вынесено в отдельный файл

--- private/bootstrap.php
+++ private/bootstrap.php
@@ -13,15 +13,8 @@ if (version_compare(PHP_VERSION, '7.2') < 0) {
     die(sprintf('Need version PHP 7.2 or higher. Your version: %s', PHP_VERSION));
 }
 
-define('DIR_ROOT', realpath(__DIR__.'/../'));
-define('DIR_CACHE', DIR_ROOT.DIRECTORY_SEPARATOR.'.cache');
-define('DIR_LOGS', DIR_ROOT.DIRECTORY_SEPARATOR.'.logs');
-define('DIR_TMP', DIR_ROOT.DIRECTORY_SEPARATOR.'.tmp');
-define('DIR_CONFIG', DIR_ROOT.DIRECTORY_SEPARATOR.'config');
-define('DIR_TWIG_TEMPLATES', DIR_ROOT.DIRECTORY_SEPARATOR.'private'.DIRECTORY_SEPARATOR.'templates');
-
 require '../vendor/autoload.php';
-
+require_once __DIR__.'/bootstrap/constants.php';
 require_once __DIR__.'/bootstrap/global_functions.php';
 
 
---
+++ private/bootstrap/constants.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * WofhTools
+ *
+ * @author      delphinpro <delphinpro@gmail.com>
+ * @copyright   copyright © 2019 delphinpro
+ * @license     licensed under the MIT license
+ */
+
+define('SEP', DIRECTORY_SEPARATOR);
+define('DIR_ROOT', realpath(__DIR__.'/../../'));
+define('DIR_CACHE', DIR_ROOT.SEP.'.cache');
+define('DIR_LOGS', DIR_ROOT.SEP.'.logs');
+define('DIR_TMP', DIR_ROOT.SEP.'.tmp');
+define('DIR_CONFIG', DIR_ROOT.SEP.'config');
+define('DIR_TWIG_TEMPLATES', DIR_ROOT.SEP.'private'.SEP.'templates');
