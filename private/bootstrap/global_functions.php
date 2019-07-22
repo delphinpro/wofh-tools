5820ecca00aa62440e00df50f6161ce5134688fa
delphinpro <delphinpro@yandex.ru>
2019-07-22 14:47:15 +0400

    Добавлена глобальнаяя функция GUID()

--- private/bootstrap/global_functions.php
+++ private/bootstrap/global_functions.php
@@ -147,3 +147,19 @@ function bootEloquent(array $settings): \Illuminate\Database\Capsule\Manager
 
     return $capsule;
 }
+
+
+function GUID()
+{
+    mt_srand((double)microtime() * 10000);
+    $charId = strtoupper(md5(uniqid(rand(), true)));
+    $hyphen = chr(45);// "-"
+    $uuid = chr(123)// "{"
+        .substr($charId, 0, 8).$hyphen
+        .substr($charId, 8, 4).$hyphen
+        .substr($charId, 12, 4).$hyphen
+        .substr($charId, 16, 4).$hyphen
+        .substr($charId, 20, 12)
+        .chr(125);// "}"
+    return $uuid;
+}
