11dec96d02d157730b44565cf52a2e20f2a176fa
delphinpro <delphinpro@yandex.ru>
2019-07-24 20:36:58 +0400

    Выпадающие меню в админке

--- src/components/Dashboard/DashboardSidebar.vue
+++ src/components/Dashboard/DashboardSidebar.vue
@@ -9,6 +9,8 @@ export default {
     name: 'DashboardSidebar',
 
     data: () => ({
+        demoUIElementsOpen: false,
+        demoWidgetsOpen: false,
     }),
 };
 </script>
@@ -22,10 +24,26 @@ export default {
                 <li class="header">WOFH</li>
                 <li class=""><router-link to="/dashboard/worlds">Worlds</router-link></li>
                 <li class="header">DEMO COMPONENTS</li>
-                <li class=""><router-link to="/dashboard/type">Typography</router-link></li>
-                <li class=""><router-link to="/dashboard/palette">Color palette</router-link></li>
-                <li class=""><router-link to="/dashboard/boxes">Boxes</router-link></li>
-                <li class=""><router-link to="/dashboard/alerts">Alerts</router-link></li>
+                <li class="treeview" :class="{'active menu-open':demoUIElementsOpen}">
+                    <a href="#" @click.prevent="demoUIElementsOpen=!demoUIElementsOpen">
+                        <i class="fa fa-laptop"></i> <span>UI Elements</span>
+                        <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li class=""><router-link to="/dashboard/palette">Color palette</router-link></li>
+                        <li class=""><router-link to="/dashboard/type">Typography</router-link></li>
+                    </ul>
+                </li>
+                <li class="treeview" :class="{'active menu-open':demoWidgetsOpen}">
+                    <a href="#" @click.prevent="demoWidgetsOpen=!demoWidgetsOpen">
+                        <i class="fa fa-th"></i> <span>Widgets</span>
+                        <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li class=""><router-link to="/dashboard/boxes">Boxes</router-link></li>
+                        <li class=""><router-link to="/dashboard/alerts">Alerts</router-link></li>
+                    </ul>
+                </li>
             </ul>
         </section>
     </aside>
