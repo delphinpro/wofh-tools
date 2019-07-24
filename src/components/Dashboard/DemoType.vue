82813832efc792b601b185870592a5f38ddb1feb
delphinpro <delphinpro@yandex.ru>
2019-07-24 04:19:11 +0400

    DL

--- src/components/Dashboard/DemoType.vue
+++ src/components/Dashboard/DemoType.vue
@@ -137,14 +137,15 @@ export default {
             </div>
         </div>
 
-        <h3 class="type-title">Description</h3>
+        <h3 class="type-title">Description List</h3>
 
         <dl>
             <dt>Description lists</dt>
             <dd>A description list is perfect for defining terms.</dd>
             <dt>Euismod</dt>
-            <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
-            <dd>Donec id elit non mi porta gravida at eget metus.</dd>
+            <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.<br>
+                Donec id elit non mi porta gravida at eget metus.
+            </dd>
             <dt>Malesuada porta</dt>
             <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
         </dl>
@@ -155,18 +156,18 @@ export default {
             <dt>Description lists</dt>
             <dd>A description list is perfect for defining terms.</dd>
             <dt>Euismod</dt>
-            <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
-            <dd>Donec id elit non mi porta gravida at eget metus.</dd>
-            <dt>Malesuada porta</dt>
+            <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.<br>
+                Donec id elit non mi porta gravida at eget metus.
+            </dd>
+            <dt>Malesuada porta lorem</dt>
             <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
-            <dt>Felis euismod semper eget lacinia</dt>
-            <dd>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo.</dd>
         </dl>
     </div>
 </template>
 
 <style lang="scss">
     .type-title {
+        @include margin-leader(1);
         position: relative;
 
         &::after {
