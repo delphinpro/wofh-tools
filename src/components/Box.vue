371e44af57d7ab8bc5ee143161fdb029904650aa
delphinpro <delphinpro@yandex.ru>
2019-07-26 00:59:36 +0400

    Добавлен компонент Box.vue
    Панель с заголовком

---
+++ src/components/Box.vue
@@ -0,0 +1,33 @@
+<script>/*!
+ * WofhTools
+ * Component: Box.vue
+ * (c) 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export default {
+    name: 'Box',
+
+    props: {
+        type: { type: String, default: '' },
+        title: { type: String },
+        icon: { type: String, default: '' },
+    },
+};
+</script>
+
+<template>
+    <div class="box2" :class="(type ? 'box2_style_'+type : '')">
+        <div class="box2__header" v-if="title">
+            <span
+                class="box2__icon"
+                :class="(icon ? 'fa fa-'+icon : '')"
+                v-if="icon"
+            ></span>
+            <span class="box2__title">{{title}}</span>
+        </div>
+        <div class="box2__body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
