0b8da2863f1874158d4675e3089c913437222057
delphinpro <delphinpro@yandex.ru>
2019-07-26 00:37:02 +0400

    Вынесены в отдельный файл стили из компонента AppLogo.vue

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -17,31 +17,3 @@ export default {
         <span class="logo__beta">beta</span>
     </router-link>
 </template>
-
-<style lang="scss">
-    .logo {
-        font-family: $family-head;
-        font-weight: 400;
-        display: flex;
-        align-items: center;
-        text-decoration: none !important;
-        color: lighten($main-color, 30%) !important;
-
-        &__image {
-            @include size(rhythm($navbar-height-rhythm - $navbar-padding-rhythm * 2));
-        }
-
-        &__name {
-            @include font(20px);
-            transform: scaleY(1.3);
-            margin-left: 0.5em;
-        }
-
-        &__beta {
-            @include font(11px);
-            position: relative;
-            top: -0.5em;
-            margin-left: 0.5em;
-        }
-    }
-</style>
