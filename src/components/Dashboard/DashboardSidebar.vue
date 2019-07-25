c50cabda914fe2e063973857576aa6833b059241
delphinpro <delphinpro@yandex.ru>
2019-07-26 01:15:06 +0400

    Добавлен компонент DashboardSidebar.vue

---
+++ src/components/Dashboard/DashboardSidebar.vue
@@ -0,0 +1,20 @@
+<script>/*!
+ * WofhTools
+ * Component: DashboardSidebar.vue
+ * © 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export default {
+    name: 'DashboardSidebar',
+};
+</script>
+
+<template>
+    <aside class="dashboard-sidebar">
+        <ul class="dashboard-menu sidebar-menu">
+            <li class="dashboard-menu-header">WOFH</li>
+            <li><router-link to="/dashboard/worlds">Worlds</router-link></li>
+        </ul>
+    </aside>
+</template>
