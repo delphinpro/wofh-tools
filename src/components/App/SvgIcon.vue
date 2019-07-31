9d150bf3900b99c474e4db024245ec375c11418b
delphinpro <delphinpro@yandex.ru>
2019-07-31 22:55:10 +0400

    Настроены svg-спрайты

---
+++ src/components/App/SvgIcon.vue
@@ -0,0 +1,55 @@
+<script>
+    export default {
+        name: 'SvgIcon',
+
+        props: {
+            name: {
+                type: String,
+                required: true,
+            },
+            color: {
+                type: String,
+                default: 'currentColor',
+            },
+            size: {
+                type: [String, Number],
+                default: null,
+            },
+        },
+
+        computed: {
+            iconPath() {
+                let icon = require(`@/assets/icons/${this.name}.svg`);
+                if (Object.prototype.hasOwnProperty.call(icon, 'default')) {
+                    icon = icon.default;
+                }
+
+                return icon.url;
+            },
+
+            className() {
+                return 'svg-icon-' + this.name;
+            },
+        },
+    };
+</script>
+
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        class="svg-icon"
+        :class="className"
+        :fill="color"
+        :width="size"
+        :height="size">
+        <!--suppress HtmlUnknownAttribute, XmlUnusedNamespaceDeclaration -->
+        <use :xlink:href="iconPath" xmlns:xlink="http://www.w3.org/1999/xlink"/>
+    </svg>
+</template>
+
+<style>
+    .svg-icon {
+        width: auto;
+        height: auto;
+    }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -53,6 +53,20 @@ module.exports = {
         },
     }),
 
+    pluginOptions: {
+        svgSprite: {
+            dir: './src/assets/icons',
+            test: /\.(svg)(\?.*)?$/,
+            loaderOptions: {
+                extract: true,
+                spriteFilename: 'static/icons.[hash:8].svg',
+            },
+            pluginOptions: {
+                plainSprite: true,
+            },
+        },
+    },
+
     chainWebpack: config => {
         config.module
             .rule('vue')
