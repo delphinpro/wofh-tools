f027ca90e5eceaf7cbbea80b8c95eb45e5735a99
delphinpro <delphinpro@yandex.ru>
2019-08-02 16:18:52 +0400

    Добавлен компонент CloseButton.vue

---
+++ src/components/Elements/CloseButton.vue
@@ -0,0 +1,28 @@
+<script>/*!
+ * WofhTools
+ * File: CloseButton.vue
+ * © 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+import 'vue-awesome/icons/times';
+
+
+export default {
+    name: 'CloseButton',
+
+    methods:{
+        onClick(e){
+            this.$emit('click', e);
+        }
+    }
+};
+</script>
+
+<template>
+    <button class="close-button" type="button" @click="onClick">
+        <FaIcon class="close-button__icon" name="times"/>
+    </button>
+</template>
+
+<style src="../@css/CloseButton.scss" lang="scss"></style>
