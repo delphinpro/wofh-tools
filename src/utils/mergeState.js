82cd7ebafe5017176feab58d7db078df9f376efd
delphinpro <delphinpro@yandex.ru>
2019-08-06 02:10:32 +0400

    Добавлены функции isServerBundle() и mergeState()

---
+++ src/utils/mergeState.js
@@ -0,0 +1,31 @@
+/*!
+ * WofhTools
+ * File: mergeState.js
+ * © 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export function isServerBundle() {
+    try {
+        return global.process.env.VUE_ENV && global.process.env.VUE_ENV === 'server';
+    } catch ( e ) { }
+
+    return false;
+}
+
+export function mergeState(defaultState, moduleName = 'default') {
+    let preloadState = [];
+
+    try {
+        if (isServerBundle()) {
+            preloadState = global.__PRELOAD_STATE__['STATE'];
+        } else {
+            preloadState = window.__PRELOAD_STATE__ ? window.__PRELOAD_STATE__ : [];
+        }
+    } catch ( e ) {}
+
+    return {
+        ...defaultState,
+        ...(preloadState.hasOwnProperty(moduleName) ? preloadState[moduleName] : {}),
+    };
+}
