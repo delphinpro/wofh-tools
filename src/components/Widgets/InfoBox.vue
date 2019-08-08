0656584e50858bff1ab784ad663a26e20c076543
delphinpro <delphinpro@yandex.ru>
2019-08-08 22:37:11 +0400

    Добавлен компонент InfoBox.vue

---
+++ src/components/Widgets/InfoBox.vue
@@ -0,0 +1,38 @@
+<script>/*!
+ * WofhTools
+ * File: InfoBox.vue
+ * © 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export default {
+    name: 'InfoBox',
+
+    props: {
+        link: { type: String, default: null },
+        theme: { type: String, default: null },
+    },
+
+    computed: {
+        tag() { return this.link ? 'a' : 'div'; },
+    },
+};
+</script>
+
+<template>
+    <component class="info-box" :class="theme ? 'info-box_theme_'+theme : ''" :is="tag" :href="link">
+        <div class="info-box__icon">
+            <slot name="info"></slot>
+        </div>
+        <div class="info-box__body">
+            <div class="info-box__content">
+                <slot name="default"></slot>
+            </div>
+            <div class="info-box__footer">
+                <slot name="footer"></slot>
+            </div>
+        </div>
+    </component>
+</template>
+
+<style lang="scss" src="../@css/InfoBox.scss"></style>
