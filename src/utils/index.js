ee54c0927d8041a200ad7edc5a613edfa32aba27
delphinpro <delphinpro@yandex.ru>
2019-08-10 23:05:56 +0400

    JS: Добавлена функция ucFirst()

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,6 +5,11 @@
  * licensed under the MIT license
  */
 
+export function ucFirst(str) {
+    if (!str || typeof str !== 'string') return str;
+    return str[0].toUpperCase() + str.slice(1);
+}
+
 export function cbSortWorldsByStarted(a, b) {
     if (a.started === b.started) return 0;
     return a.started < b.started ? 1 : -1;
