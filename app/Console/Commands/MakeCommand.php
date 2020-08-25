fdcfb1fcbbfccbd6c8a4a19968dddcd6f4d70555
delphinpro <delphinpro@yandex.ru>
2020-08-25 19:44:27 +0400

    Функционал вывода списка группы команд вынесен в трейт

--- app/Console/Commands/MakeCommand.php
+++ app/Console/Commands/MakeCommand.php
@@ -10,16 +10,12 @@
 namespace App\Console\Commands;
 
 
-use App\Console\Color;
-use App\Console\Traits\ConsoleColors;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Str;
 
 
 class MakeCommand extends Command
 {
-    use ConsoleColors;
+    use ListCommands;
 
 
     protected $signature = 'make';
@@ -27,79 +23,11 @@ class MakeCommand extends Command
     protected $description = 'List all make commands';
 
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-
     public function handle()
     {
         $this->comment('');
         $this->comment('Available commands:');
 
-        $this->listMakeCommands();
-    }
-
-
-    /**
-     * List all make commands.
-     *
-     * @return void
-     */
-    protected function listMakeCommands()
-    {
-        $commands = collect(Artisan::all())->mapWithKeys(function ($command, $key) {
-            if (Str::startsWith($key, 'make:')) {
-                return [$key => $command];
-            }
-
-            return [];
-        })->toArray();
-
-        $width = $this->getColumnWidth($commands);
-
-        /** @var Command $command */
-        foreach ($commands as $command) {
-            $name = $this->makeString(str_pad($command->getName(), $width), Color::GREEN);
-            $this->line(' '.$name.$command->getDescription());
-        }
-    }
-
-
-    /**
-     * @param (Command|string)[] $commands
-     *
-     * @return int
-     */
-    private function getColumnWidth(array $commands)
-    {
-        $widths = [];
-
-        foreach ($commands as $command) {
-            $widths[] = static::strlen($command->getName());
-            foreach ($command->getAliases() as $alias) {
-                $widths[] = static::strlen($alias);
-            }
-        }
-
-        return $widths ? max($widths) + 2 : 0;
-    }
-
-
-    /**
-     * Returns the length of a string, using mb_strwidth if it is available.
-     *
-     * @param string $string The string to check its length
-     *
-     * @return int The length of the string
-     */
-    public static function strlen($string)
-    {
-        if (false === $encoding = mb_detect_encoding($string, null, true)) {
-            return strlen($string);
-        }
-
-        return mb_strwidth($string, $encoding);
+        $this->allCommands('make');
     }
 }
