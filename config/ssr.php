24001fb496bb51041f983abe4c0523a504ff3430
delphinpro <delphinpro@yandex.ru>
2020-08-28 15:24:32 +0400

    Fix: SSR config

--- config/ssr.php
+++ config/ssr.php
@@ -4,6 +4,7 @@ return [
     /*
      * Enable or disable the server renderer. Enabled in production by default.
      */
+
     'enabled' => env('SSR_ENABLED', false),
 
     /*
@@ -12,12 +13,14 @@ return [
      * `debug` is enabled, an exception will be thrown when the JavaScript can't
      * be executed.
      */
+
     'debug' => env('APP_DEBUG', false),
 
     /*
      * Set to true if you're using Laravel Mix, then you can pass a script
      * identifier to `ssr` instead of a full path.
      */
+
     'mix' => true,
 
     /*
@@ -25,14 +28,16 @@ return [
      * up some extra configuration below. If you want to use the V8 engine, make
      * sure the v8js php extension is available.
      */
+
     'engine' => env('SSR_ENGINE', \Spatie\Ssr\Engines\Node::class),
 
     /*
      * Extra setup for the Node engine.
      */
+
     'node' => [
         'node_path' => env('NODE_PATH', '/usr/local/bin/node'),
-        'temp_path' => env('SSR_TEMP_PATH', storage_path('app/ssr')),
+        'temp_path' => storage_path(env('SSR_TEMP_PATH', 'app/ssr')),
     ],
 
     /*
@@ -40,6 +45,7 @@ return [
      * data you *always* want to send to the server script. Context can contain
      * anything that's json serializable.
      */
+
     'context' => [],
 
     /*
@@ -51,8 +57,9 @@ return [
      * By default, env is prefilled with some necessary values for server side
      * rendering Vue applications.
      */
+
     'env' => [
         'NODE_ENV' => 'production',
-        'VUE_ENV' => 'server',
+        'VUE_ENV'  => 'server',
     ],
 ];
