838f6a44a44fa5f1690a2d482dc4a03d7aec1623
delphinpro <delphinpro@yandex.ru>
2020-09-13 21:17:24 +0400

    Refactor: Axios utils

--- src/entry-client.js
+++ src/entry-client.js
@@ -9,47 +9,42 @@ import Vue from 'vue';
 import Toast from 'cxlt-vue2-toastr';
 import axios from 'axios';
 import { createApp } from '@/app';
-import VueAxiosBridge from '@/utils/vue-axios';
+import VueAxiosBridge, { requestSuccess, requestFailed, responseSuccess, responseFailed } from '@/utils/axios.js';
 import { HTTP_HEADER_AUTHORIZATION, LS_KEY_TOKEN } from '@/constants';
-import responseSuccess from '@/utils/axios-response-success';
-import responseFailed from '@/utils/axios-response-failed';
-import { requestFailed, requestSuccess } from '@/utils/axios-request';
-
 
 //==
 //== Toast
 //== ======================================= ==//
 
 Vue.use(Toast, {
-  position: 'top right',
-  closeButton: true,
+  position    : 'top right',
+  closeButton : true,
   // progressBar: true,
-  useHtml: true,
-  type: 'success',
-  showMethod: 'lightSpeedIn',
-  hideMethod: 'slideOutRight',
+  useHtml     : true,
+  type        : 'success',
+  showMethod  : 'lightSpeedIn',
+  hideMethod  : 'slideOutRight',
   showDuration: 300,
   hideDuration: 200,
-  timeOut: 10000,
+  timeOut     : 10000,
 });
 Vue.$toast = Vue.prototype.$toast;
-
 
 //==
 //== Axios: create instance
 //== ======================================= ==//
 
 Vue.use(VueAxiosBridge, axios.create({
-  baseURL: '/api',
-  timeout: 0,
-  responseType: 'json',
+  baseURL         : '/api',
+  timeout         : 0,
+  responseType    : 'json',
   responseEncoding: 'utf8',
-  headers: {
+  headers         : {
     'X-Requested-With': 'XMLHttpRequest',
   },
 
   // Reject only if the status code is greater than or equal to 500
-  validateStatus: status => status < 500,
+  validateStatus: status => status < 400,
 }));
 
 Vue.axios.interceptors.request.use(requestSuccess, requestFailed);
@@ -58,13 +53,11 @@ Vue.axios.interceptors.response.use(responseSuccess, responseFailed);
 let token = localStorage.getItem(LS_KEY_TOKEN);
 if (token) Vue.axios.defaults.headers.common[HTTP_HEADER_AUTHORIZATION] = `Bearer ${token}`;
 
-
 //==
 //== Main app
 //== ======================================= ==//
 
 createApp(window.__STATE__).$mount('#app');
-
 
 //==
 //== Debug bar
---
+++ src/utils/axios.js
@@ -0,0 +1,136 @@
+/*!
+ * WofhTools
+ * (c) 2019-2020 delphinpro <delphinpro@yandex.ru>
+ * licensed under the MIT license
+ */
+
+import Vue from 'vue';
+import { colors } from '@/utils/console-tools.js';
+import trimRootPath from '@/utils/trimRootPath.js';
+
+/**
+ * Vue Axios plugin
+ *
+ * author:     https://github.com/imcvampire
+ * repository: https://github.com/imcvampire/vue-axios
+ * licensed    under the MIT license
+ *
+ * @param Vue
+ * @param axios
+ */
+export default function plugin(Vue, axios) {
+
+  if (plugin.installed) {
+    return;
+  }
+
+  plugin.installed = true;
+
+  if (!axios) {
+    console.error('You have to install axios');
+    return;
+  }
+
+  Vue.axios = axios;
+
+  Object.defineProperties(Vue.prototype, {
+    axios: {
+      get() { return axios; },
+    },
+  });
+}
+
+/*==
+ *== Request interceptors
+ *== ======================================= ==*/
+
+export function requestSuccess(config) {
+  if (process.env.NODE_ENV === 'development') {
+
+    console.log(`%c<Interceptor> Request [${config.baseURL + config.url}]`, colors.warn);
+
+  }
+  return config;
+}
+
+export function requestFailed(error) {
+  if (process.env.NODE_ENV === 'development') {
+
+    // Todo: Do something with request error
+    console.log('<Interceptor> Request Error: ', error);
+
+  }
+  return Promise.reject(error);
+}
+
+/*==
+ *== Response interceptors
+ *== ======================================= ==*/
+
+export function responseSuccess(response) {
+
+  console.log(response);
+  if (response.data.status !== false) {
+
+    if (response.data.message) {
+      Vue.$toast.success({
+        title  : 'Success',
+        message: response.data.message,
+      });
+    }
+
+    return response.data.payload;
+
+  } else {
+
+    Vue.$toast.warn({
+      title  : response.data.message,
+      message: `Request [${response.config.method.toUpperCase()}] ${response.config.url}`,
+    });
+
+  }
+
+  console.log('<Interceptor> Response', response.data);
+  return response;
+}
+
+export function responseFailed(error) {
+
+  let httpStatusCode = error.response.status;
+  let httpStatusText = error.response.statusText;
+  let title = `${httpStatusCode} ${httpStatusText}`;
+  let message = '';
+
+  if (process.env.NODE_ENV === 'development') {
+
+    console.log(`%c<Interceptor> Response Error ${httpStatusCode} with message: ${error.response.data.message}`, colors.warn);
+
+    message = `Request [${error.config.method.toUpperCase()}] ${error.config.url}`;
+
+    let err = {};
+    if (error.response.data.exception) {
+      err = error.response.data.exception[0];
+    } else if (error.response.data.error) err = error.response.data.error[0];
+
+    if (err.message) {
+      err.file = trimRootPath(err.file);
+      err.trace = err.trace.map(trimRootPath);
+      message += `<br>${error.response.data.message}.`;
+      message += `<br><b>${err.message}.</b><br>`;
+      message += `<br>${err.file}:${err.line}.<br>`;
+      message += `<br>See console.`;
+
+      console.groupCollapsed(`%c${err.type}: ${err.message}`, colors.danger);
+      console.log(`%cFile:    ${err.file}:${err.line}`, colors.info);
+      err.trace.map(item => console.log(trimRootPath(item)));
+      console.groupEnd();
+    }
+  }
+
+  Vue.$toast.error({
+    title,
+    message,
+  });
+
+  return Promise.reject(error);
+}
