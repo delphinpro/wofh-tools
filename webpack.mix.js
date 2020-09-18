6bff2cf1b7071fdd1ac74855d9790c2d4eaab437
delphinpro <delphinpro@yandex.ru>
2020-09-18 18:21:50 +0400

    AppLogo

--- webpack.mix.js
+++ webpack.mix.js
@@ -40,18 +40,6 @@ if (IS_CLIENT) {
     path.join(__dirname, `resources/assets/images`),
     path.join(__dirname, `public/${dist}/images`),
   );
-  mix.copy(
-    path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free/sprites/regular.svg'),
-    path.join(__dirname, `public/${dist}/regular.svg`),
-  );
-  mix.copy(
-    path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free/sprites/solid.svg'),
-    path.join(__dirname, `public/${dist}/solid.svg`),
-  );
-  mix.copy(
-    path.join(__dirname, `resources/assets/custom.svg`),
-    path.join(__dirname, `public/${dist}/custom.svg`),
-  );
 }
 
 if (IS_SERVER) {
